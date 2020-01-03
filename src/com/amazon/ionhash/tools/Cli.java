9c1fcaf31d65ef354d8c62e0f42734fc9d7d66da
Peter Cornell <pcornell@amazon.com>
2020-01-03 15:01:05 -0800

    improvements to the cli tool

--- src/com/amazon/ionhash/tools/Cli.java
+++ src/com/amazon/ionhash/tools/Cli.java
@@ -1,42 +1,48 @@
 package com.amazon.ionhash.tools;
 
 import com.amazon.ion.IonReader;
+import com.amazon.ion.IonType;
 import com.amazon.ion.system.IonReaderBuilder;
 import com.amazon.ionhash.IonHashReader;
 import com.amazon.ionhash.IonHashReaderBuilder;
 import com.amazon.ionhash.MessageDigestIonHasherProvider;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 
 public class Cli {
     public static void main(String[] args) throws Exception {
-        String algorithm = args[0];
-
-        InputStream in = System.in;
-        if (args.length > 1) {
-            in = new FileInputStream(args[1]);
+        if (args.length < 2) {
+            System.out.println("Utility that prints the Ion Hash of the top-level values in a file.");
+            System.out.println();
+            System.out.println("Usage:");
+            System.out.println("  ion-hash [algorithm] [filename]");
+            System.out.println();
+            System.out.println("where [algorithm] is a hash function such as sha-256");
+            System.out.println();
+            System.exit(1);
         }
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        String line;
-        while ((line = br.readLine()) != null) {
-            try {
-                IonHashReader ihr = IonHashReaderBuilder.standard()
-                        .withReader(IonReaderBuilder.standard().build(line))
-                        .withHasherProvider(new MessageDigestIonHasherProvider(algorithm))
-                        .build();
-                ihr.next();
-                ihr.next();
-                System.out.println(bytesToHex(ihr.digest()));
-            } catch (Exception e) {
-                System.out.println("[unable to digest: " + e + "]");
+        String algorithm = args[0];
+        String fileName = args[1];
+
+        try (InputStream inputStream = new FileInputStream(fileName);
+             IonReader reader = IonReaderBuilder.standard().build(inputStream);
+             IonHashReader hashReader = IonHashReaderBuilder.standard()
+                     .withReader(reader)
+                     .withHasherProvider(new MessageDigestIonHasherProvider(algorithm))
+                     .build()) {
+
+            IonType type = hashReader.next();
+            while (type != null) {
+                try {
+                    type = hashReader.next();
+                    System.out.println(bytesToHex(hashReader.digest()));
+                } catch (Exception e) {
+                    System.out.println("[unable to digest: " + e + "]");
+                }
             }
         }
-        br.close();
     }
 
     private static String bytesToHex(byte[] bytes) {
@@ -44,7 +50,7 @@ public class Cli {
         for (byte b : bytes) {
             sb.append(String.format("%02x ", b));
         }
-        return sb.toString();
+        return sb.toString().trim();
     }
 }
 
